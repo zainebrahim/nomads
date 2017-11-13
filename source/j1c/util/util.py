cbb68b7186904c52d38ca751e4d65cac03443aba
j1c <jaewonc78@gmail.com>
2017-11-12 21:41:12 -0500

    faster code

--- source/j1c/util/util.py
+++ source/j1c/util/util.py
@@ -1,6 +1,48 @@
 import numpy as np
+from scipy.sparse import csr_matrix
 
 
+def bounding_box2(img, fname=None):
+    """
+    Returns all z, y, x vectors that create a bounding box
+    of a mask.
+
+    Parameters
+    ----------
+    img : 3d-array like
+        Sparsely labeled image (e.g. annotation)
+    fname : string, optional
+        File or file path to which the data is saved. The file
+        is saved as a numpy array.
+
+    Returns
+    -------
+    centroids : ndarray
+        List of all centroids in format 
+            (label, zmin, zmax, ymin, ymax, xmin, xmax)
+    """
+
+    sp_arr = csr_matrix(img.reshape((1, -1)))
+    uniques = np.unique(sp_arr.data)
+
+    bounds = np.zeros((len(uniques), 7), dtype=np.int)
+
+    for i, label in enumerate(uniques):
+        z, y, x = np.unravel_index(
+            sp_arr.indices[sp_arr.data == label], img.shape)
+
+        zmin, zmax = np.min(z), np.max(z)
+        ymin, ymax = np.min(y), np.max(y)
+        xmin, xmax = np.min(x), np.max(x)
+
+        bounds[i] = label, zmin, zmax + 1, ymin, ymax + 1, xmin, xmax + 1
+
+    if fname:
+        np.save(fname, bounds)
+    else:
+        return bounds
+
+'''
 def bounding_box(img):
     """
     Returns the z, y, x vectors that create a bounding box
@@ -15,7 +57,7 @@ def bounding_box(img):
     xmin, xmax = np.where(x)[0][[0, -1]]
 
     return (zmin, zmax + 1), (ymin, ymax + 1), (xmin, xmax + 1)
-
+'''
 
 def get_uniques(ar):
     """
